3b245a6627d9fecc63523cb15a9f4e176c7cc86b
Bram-Wel <welbram@gmail.com>
2023-01-12 21:29:59 +0300

    insertion sort.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+* insertion_sort_list - Sorts a list of integers
+*
+* @list: List to sort
+* Description: Doubly linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list, *node, *temp;
+	
+	while (head->next)
+	{
+		temp = head;
+		node = head->next;
+		if (head->n > node->n)
+		{	
+			while (temp && temp->n > node->n)
+			{
+				temp->next = node->next;
+				if (temp->next)
+					temp->next->prev = temp;
+				node->prev = temp->prev;
+				if (node->prev)
+					node->prev->next = node;
+				else
+					*list = node;
+				temp->prev = node;
+				node->next = temp;
+				print_list(*list);
+				temp = node->prev;
+			}
+			continue;
+		}
+		head = head->next;
+	}
+}
